36c2160514a63aec6c21f2ffba860589064dd515
Nimira43 <n.rabone72@aol.com>
2026-05-16 09:01:58 +0100

    Setting up initial roots and Json server.

--- logos-ui/src/routes/__root.tsx
+++ logos-ui/src/routes/__root.tsx
@@ -1,16 +1,28 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import '../styles.css'
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        name: 'description',
+        content: 'Share, explore, collaborate and build on startup ideas and projects.'
+      },
+      {
+        title: 'Logos - Your Creation Platform'
+      }
+    ]
+  }),
   component: RootComponent,
 })
 
 function RootComponent() {
   return (
     <>
+      <HeadContent />
       <Outlet />
       <TanStackDevtools
         config={{
---
+++ logos-ui/src/routes/ideas/$ideaId/index.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/ideas/$ideaId/')({
+  component: IdeaDetailsPage
+  ,
+})
+
+function IdeaDetailsPage
+() {
+  return <div>Hello "/ideas/$ideaId/"!</div>
+}
